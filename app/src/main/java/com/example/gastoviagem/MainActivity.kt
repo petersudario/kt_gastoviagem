9446c0e5af25691eb11ae6afeec1cf932d4ba5fe
Pedro Sudario <pedrohenriquesudariosilva@gmail.com>
2024-09-14 23:21:55 -0300

    A Toast usage

--- app/src/main/java/com/example/gastoviagem/MainActivity.kt
+++ app/src/main/java/com/example/gastoviagem/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.gastoviagem
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import com.example.gastoviagem.databinding.MainBinding
 
@@ -11,5 +12,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         binding = MainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.buttonCalculate.setOnClickListener {
+            calculate()
+        }
+    }
+
+    private fun calculate() {
+        Toast.makeText(this, "Calculado", Toast.LENGTH_LONG).show()
     }
 }
